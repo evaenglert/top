886e272575e2290e05087b92681c3edc1c80a282
Franciska Englert <englert.franciska@gmail.com>
2022-02-28 18:29:00 +0000

    Added basic logic for game

--- rk-pp-sci/app.js
+++ rk-pp-sci/app.js
@@ -1,1 +1,20 @@
 console.log("Hello world!")
+console.log(computerPlay())
+
+function computerPlay() {
+  const array = ["Rock", "Paper", "Scissors"];
+  const number = Math.floor(Math.random() * 3);
+  return array[number];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.lower() === computerSelection.lower()) {
+    return "It is a tie!";
+  }
+  else if ((playerSelection.lower() === "rock" && computerSelection === "Scissors") || (playerSelection.lower() === "paper" && computerSelection === "Rock") ||(playerSelection.lower() === "scissors" && computerSelection === "Paper")) {
+    return "Player wins!";
+  }
+  else {
+    return "Computer wins!";
+  }
+}
