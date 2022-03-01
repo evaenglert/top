ecbc9e7f15045b566cd57f4f8b63096e2cadd9f0
Franciska Englert <englert.franciska@gmail.com>
2022-03-01 09:39:42 +0000

    Finished rock paper scissors

--- rk-pp-sci/app.js
+++ rk-pp-sci/app.js
@@ -8,13 +8,43 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.lower() === computerSelection.lower()) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     return "It is a tie!";
   }
-  else if ((playerSelection.lower() === "rock" && computerSelection === "Scissors") || (playerSelection.lower() === "paper" && computerSelection === "Rock") ||(playerSelection.lower() === "scissors" && computerSelection === "Paper")) {
+  else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") || (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") || (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")) {
     return "Player wins!";
   }
   else {
     return "Computer wins!";
   }
 }
+
+function game(rounds = 5) {
+  currentPointsPlayer = 0
+  currentPointsComp = 0
+  for (let i = 0; i < rounds; i++) {
+    player = prompt("Please select between Rock, Paper, Scrissors!");
+    computer = computerPlay();
+    result = playRound(player, computer);
+    console.log(result);
+    if (result === "Player wins!") {
+      currentPointsPlayer += 1;
+    }
+    else {
+      currentPointsComp += 1;
+    }
+    console.log("Current Points - Player: " + currentPointsPlayer + " Computer: " + currentPointsComp);
+  }
+
+  if (currentPointsPlayer > currentPointsComp) {
+    console.log("Player won the whole game, congrats!!");
+  }
+  else if (currentPointsPlayer < currentPointsComp) {
+    console.log("Unfortunately the computer won the game! Try again!");
+  }
+  else {
+    console.log("Well this is a tie! Great game both!");
+  }
+}
+
+game()
