ffe029c82230f4eb4f0ca967be28e1eb9a993498
Franciska Englert <englert.franciska@gmail.com>
2022-04-01 10:05:41 +0100

    Added black and colorful options. Not working properly yet

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,31 +1,53 @@
-// create 16 * 16 divs in a grid :)
 
-
+const body = document.querySelector("body");
 const outer_grid = document.querySelector('.outer-grid');
-console.log(outer_grid)
+const reset_button = document.querySelector('.reset')
+const slider = document.querySelector(".slider");
+const slider_value = document.querySelector('.slider-value');
+const black_button = document.querySelector(".option-black")
+const random_color_button = document.querySelector(".option-colorful")
+
+enableDrawing(slider.value)
 
 
 
-var slider = document.getElementById("myRange");
-var output = document.querySelector("body");
-// console.log(output)
-const demo = document.createElement('div');
-output.appendChild(demo) // Display the default slider value
-demo.innerHTML = slider.value
-// // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  demo.innerHTML = this.value;
-  N = this.value
+slider_value.innerHTML = slider.value
 
-  console.log(N)
+enableSlider()
 
+black_button.addEventListener('click', () => {
+  enableDrawing('black')
+})
+
+random_color_button.addEventListener('click', () => {
+  enableDrawing('colorful')
+})
+
+
+// Update the current slider value (each time you drag the slider handle)
+
+
+
+// FUNCTIONS
+
+function add_reset_button(all_grid) {
+  reset_button.addEventListener('click', () => {
+    console.log(all_grid)
+    for (var i = 0; i < all_grid.length; i++) {
+      all_grid[i].classList.remove('black')
+    }
+  })
+}
+
+function createGrid(N, color_option) {
+
+  // remove any current grids
   const rows = document.querySelectorAll('.row');
-
   rows.forEach(row => {
     row.remove();
   });
 
-
+  // create grid
   for (var i = 0; i < N; i++) {
     const div_outer = document.createElement('div');
     div_outer.classList.add('row');
@@ -42,21 +64,35 @@ slider.oninput = function () {
     }
   }
 
+}
+
+
+function enableDrawing(color_option) {
   const all_grid = document.querySelectorAll('.grid');
 
+  // add coloring functionality to each grid
   for (var i = 0; i < all_grid.length; i++) {
     all_grid[i].addEventListener('mouseenter', (e) => {
-      e.target.classList.add('colored')
+      if (color_option == 'black') {
+        e.target.classList.add('black')
+      }
+      else {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        e.target.style.backgroundColor = '#' + randomColor;
+      }
     });
   }
 
+  add_reset_button(all_grid)
+}
 
-  const reset_button = document.querySelector('.reset')
+function enableSlider() {
 
-  reset_button.addEventListener('click', () => {
-    for (var i = 0; i < all_grid.length; i++) {
-      all_grid[i].classList.remove('colored')
-    }
-  })
 
+  slider.oninput = function () {
+    slider_value.innerHTML = this.value;
+    N = this.value
+
+    createGrid(N)
+  }
 }
