cc46b56fd4d0711075a94e3e7071c1de4e481212
Franciska Englert <englert.franciska@gmail.com>
2022-07-15 10:45:13 +0100

    Started adding interactivity - button click to display, evaluate

--- calculator/app.js
+++ calculator/app.js
@@ -1,3 +1,8 @@
+const display = document.querySelector('.calculator-display-number');
+const buttons = document.querySelectorAll('.button-to-display');
+const equal_button = document.querySelector('.equal');
+
+
 // some functions for basic functionality of the calculator
 
 
@@ -23,3 +28,29 @@ function operate(operator, a, b) {
 
   return operators[operator](a,b)
 }
+
+const displayNumber = function (e) {
+  display.textContent = display.textContent + e.target.textContent
+}
+
+const evalualte = function (e) {
+  expression = display.textContent
+  all_operators = expression.match(/[/*+-]/g)
+
+  numbers = expression.split(/[/*+-]/g)
+
+  if (numbers.indexOf('') != -1) {
+    display.textContent = 'ERROR'
+  }
+  else {
+    result = operate(all_operators[0], parseInt(numbers[0]), parseInt(numbers[1]))
+    display.textContent = result;
+
+  }
+
+
+}
+
+buttons.forEach(button => button.addEventListener("click", displayNumber));
+
+equal_button.addEventListener("click", evalualte);
